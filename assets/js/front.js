ad8c9989d44b07838d8af5ca956bb721e75b385f
Vika <vorona.v.w@gmail.com>
2021-05-30 18:50:59 +0300

    Application slider tab section

--- assets/js/front.js
+++ assets/js/front.js
@@ -11,12 +11,56 @@ for (let smoothLink of smoothLinks) {
     });
 }
 
-$(document).ready(function () {
-    $('.applications-slider').slick({
+(function($) {
+    const $appTabSlider = $('.applications-slider');
+    const $appContentSlider = $('.iphone-content-wrap');
+
+    reinitSlider($appTabSlider, {
         dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        variableWidth: true
+        arrows: false,
+        variableWidth: true,
+        focusOnSelect: true
     });
-});
+    reinitSlider($appContentSlider, {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false
+    });
+
+    $appTabSlider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        contentShowBlock(nextSlide, 'app-content-js');
+        reinitSlider('.iphone-content-wrap');
+    });
+    $appContentSlider.on('beforeChange', function(event, slick, currentSlide, nextSlide){contentShowBlock(nextSlide, 'app-desc-js');});
+
+    function reinitSlider(selector, options) {
+        let $slider = $(selector);
+
+        if ($slider.hasClass('slick-slider')) {
+            $slider.slick('unslick');
+        }
+
+        $slider.not('.slick-initialized').slick(options);
+    }
+
+    function contentShowBlock(index, wrapName) {
+        let $tabContents = $('.' + wrapName);
+
+        $tabContents.hide();
+        $tabContents.filter("[data-id=" + index + "]").css('display', 'block');
+    }
+
+    function tabs() {
+        let wrapName = 'app-content-js';
+        contentShowBlock(0, wrapName);
+    }
+
+    function contents() {
+        let wrapName = 'app-desc-js';
+        contentShowBlock(0, wrapName);
+    }
+
+    tabs();
+    contents();
+
+})(jQuery);
